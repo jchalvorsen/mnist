02cebf151823df784c446e9653699615e7bb27e9
Jon Christian Halvorsen <jchalvorsen@gmail.com>
2015-11-06 11:56:37 +0100

    More basic tests

--- test_basics.py
+++ test_basics.py
@@ -1,6 +1,25 @@
 __author__ = 'Jon Christian'
 
 from mnist_basics import *
+import time
+
 
 show_avg_digit(3)
-input()
+
+
+
+data, number = load_mnist()
+
+print(data[0])
+show_digit_image(data[0])
+print(number[0])
+
+flat = flatten_image(data[0])
+
+print(flat)
+
+show_digit_image(reconstruct_image(flat))
+
+time.sleep(2)
+
+quicktest()
